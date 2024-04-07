804338c89979888e564fc4c05385fa8eabc744c9
Yogeshbairwa <ybairwa786@gmail.com>
2024-04-07 09:50:50 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './AppV13';
+import App from './AppV10C';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/useMovies.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+
+const KEY = '5b697c6c';
+
+export function useMovies(query, callback) {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('')
+
+
+    useEffect(
+
+        callback?.()
+        function () {
+            async function fetchMovies() {
+                try {
+                    setIsLoading(true);
+                    setError('')
+                    const res = await
+                        fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+
+                    if (!res.ok) throw new Error('Something went wrong with fetching Movies')
+                    const data = await res.json();
+
+                    if (data.Response === 'False') throw new Error('Movie not Found')
+
+
+                    setMovies(data.Search)
+                    setError('');
+
+                } catch (err) {
+                    setError(err.message)
+                } finally {
+                    setIsLoading(false);
+                }
+            }
+            if (query.length < 3) {
+                setMovies([])
+                setError('')
+                return;
+            }
+            // handleCloseMovie()
+            fetchMovies()
+            // return function () {
+            //     controller.abort()
+            // }
+        }, [query]
+
+    )
+    return { movies, error, isLoading }
+}
